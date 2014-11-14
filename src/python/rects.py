8512b68d2b8db9dc78f9c14993513bd0b28caa80
Joey Geralnik <jgeralnik@gmail.com>
2014-11-14 07:24:05 +0200

    Once again redo the color algorithm

--- src/python/rects.py
+++ src/python/rects.py
@@ -4,12 +4,13 @@ import numpy as np
 import random
 import sys
 
-def get_rects(img, th1=30, th2=150):
+def get_rects(img, th1=40, th2=120):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray,th1,th2,apertureSize = 3, L2gradient=True)
 
     mat = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
     dilated = cv2.dilate(edges, mat)
+
 
     contours, hierarchy = cv2.findContours(dilated,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 
@@ -28,8 +29,9 @@ if __name__ == "__main__":
 
     for f in sys.argv[1:]:
         img = cv2.imread(f)
+        cimg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         for ul, br in get_rects(img):
-            i = img[ul[1]:br[1], ul[0]:br[0]]
+            i = cimg[ul[1]:br[1], ul[0]:br[0]]
             res = detect_light_state(i)
             if res == 'green':
                 col = (0, 255, 0)
